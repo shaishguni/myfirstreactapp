7dfc8d1c5057b0f3e5f790b996b0df7c1c2a4f54
Bishworaj Poudel <iambrp.tech@gmail.com>
2020-07-16 14:35:53 +0545

    Updates

---
+++ precache-manifest.d850d875e8c2fb2c919aab16989f93d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7806e5a03b7545f39ccfb074b5f01259",
+    "url": "/myfirstreactapp/index.html"
+  },
+  {
+    "revision": "28918d6830f3ff8aa241",
+    "url": "/myfirstreactapp/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "192cdd9680cdca191cf9",
+    "url": "/myfirstreactapp/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "28918d6830f3ff8aa241",
+    "url": "/myfirstreactapp/static/js/2.dd383487.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/myfirstreactapp/static/js/2.dd383487.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "192cdd9680cdca191cf9",
+    "url": "/myfirstreactapp/static/js/main.1a818430.chunk.js"
+  },
+  {
+    "revision": "db7cc37f888ba7fa1c7b",
+    "url": "/myfirstreactapp/static/js/runtime-main.564bbf94.js"
+  }
+]);
